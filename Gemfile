834f97846cbea1ee3fc65131385cd1e42bbac49c
Jan Dudulski <jan@dudulski.pl>
2013-09-11 23:13:00 +0200

    Some config and layout in slim

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source "https://rubygems.org"
 
 gem "middleman", "~> 3.1.1"
 gem "middleman-blog", "~> 3.3.0"
+gem "middleman-livereload"
+
 gem "slim"
 
 # For feed.xml.builder
--- config.rb
+++ config.rb
@@ -2,12 +2,12 @@
 # Blog settings
 ###
 
-# Time.zone = "UTC"
+Time.zone = "Warsaw"
 
 activate :blog do |blog|
   # blog.prefix = "blog"
   # blog.permalink = ":year/:month/:day/:title.html"
-  # blog.sources = ":year-:month-:day-:title.html"
+  blog.sources = "blog/:year-:month-:day-:title.html"
   # blog.taglink = "tags/:tag.html"
   # blog.layout = "layout"
   # blog.summary_separator = /(READMORE)/
@@ -15,7 +15,7 @@ activate :blog do |blog|
   # blog.year_link = ":year.html"
   # blog.month_link = ":year/:month.html"
   # blog.day_link = ":year/:month/:day.html"
-  # blog.default_extension = ".markdown"
+  blog.default_extension = ".markdown"
 
   blog.tag_template = "tag.html"
   blog.calendar_template = "calendar.html"
@@ -27,31 +27,18 @@ end
 
 page "/feed.xml", :layout => false
 
-### 
-# Compass
-###
-
-# Susy grids in Compass
-# First: gem install susy
-# require 'susy'
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
 ###
 # Page options, layouts, aliases and proxies
 ###
 
 # Per-page layout changes:
-# 
+#
 # With no layout
 # page "/path/to/file.html", :layout => false
-# 
+#
 # With alternative layout
 # page "/path/to/file.html", :layout => :otherlayout
-# 
+#
 # A path which all have the same layout
 # with_layout :admin do
 #   page "/admin/*"
@@ -61,6 +48,8 @@ page "/feed.xml", :layout => false
 # page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
 #   @which_fake_page = "Rendering a fake page with a variable"
 # end
+
+activate :directory_indexes
 
 ###
 # Helpers
@@ -76,31 +65,33 @@ page "/feed.xml", :layout => false
 #   end
 # end
 
+configure :development do
+  activate :livereload, apply_js_live: false
+end
+
 set :css_dir, 'stylesheets'
-
 set :js_dir, 'javascripts'
-
 set :images_dir, 'images'
 
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
-  
-  # Minify Javascript on build
-  # activate :minify_javascript
-  
+  set :is_development, false
+
+  activate :minify_html
+  activate :minify_css
+  activate :minify_javascript
+
   # Enable cache buster
   # activate :cache_buster
-  
+
   # Use relative URLs
-  # activate :relative_assets
-  
+  activate :relative_assets
+
   # Compress PNGs after build
   # First: gem install middleman-smusher
   # require "middleman-smusher"
   # activate :smusher
-  
+
   # Or use a different image path
   # set :http_path, "/Content/images/"
 end
